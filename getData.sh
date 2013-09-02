fcf736affec91eee1876463c671ff1d9e27189b1
Savant Krishna <savant.2020@gmail.com>
2013-09-03 03:44:13 +0530

    added script to get data from microsoft

---
+++ getData.sh
@@ -0,0 +1,3 @@
+wget "http://ftp.research.microsoft.com/downloads/b16d359d-d164-469e-9fd4-daa38f2b2e13/Geolife Trajectories 1.3.zip?"
+unzip "Geolife Trajectories 1.3.zip"
+mv "Geolife Trajectories 1.3/" data/

49c47f6316d1a582f57be4e77ac9cae913eb7e2c
Savant Krishna <savant.2020@gmail.com>
2013-09-30 16:39:29 +0530

    added distance calculating function

--- src/gpstraces.py
+++ src/gpstraces.py
@@ -1,25 +1,47 @@
 #!/usr/bin/python
 # Author : Savant Krishna <savant.2020@gmail.com>
 
+import math as math
+
 class Point:
     def __init__(self,lat=0.0,lng=0.0,alt=0.0):
         self.lat = lat
         self.lng = lng
         self.alt = alt
+
     def getLat(self):
         return self.lat
+
     def getLng(self):
         return self.lng
+
     def getLatLng(self):
         return (self.getLat(),self.getLang())
+
     def getCopy(self):
         p = Point()
         p.lat = self.lat
         p.lng = self.lng
         p.alt = self.alt
         return p
+
     def printMe(self):
         print "Lat: ",self.lat,", Lng:",self.lng,", Alt:",self.alt
+
+    # Haversine formula example in Python
+    # Author: Wayne Dyck
+    def getDistance(self,p):
+        lat1, lon1 = [self.lat,self.lng]
+        lat2, lon2 = [p.lat,p.lng]
+        radius = 6371.0 # km
+
+        dlat = math.radians(lat2-lat1)
+        dlon = math.radians(lon2-lon1)
+        a = math.sin(dlat/2) * math.sin(dlat/2) + math.cos(math.radians(lat1)) * math.cos(math.radians(lat2)) * math.sin(dlon/2) * math.sin(dlon/2)
+        c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+        d = radius * c
+
+        return d
 
 class Trace:
     def __init__(self):
